e97a3066ddcfae17574c9d1abdcdc7a9f4cc651b
Lukas Rodrigues <lukkasdev@gmail.com>
2023-12-22 12:39:18 -0300

    Scenes, Collisions and Rocket model alterations
    22/12/2023
    2- Commit

--- RocketBoost/Assets/scripts/collisionHandler.cs
+++ RocketBoost/Assets/scripts/collisionHandler.cs
@@ -5,27 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class collisionHandler : MonoBehaviour{
 
-    void Start(){
+    movement movement;
+    AudioSource rocketAudio;
+    bool crashedOrFinished = false;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip winSound;
 
+    void Start(){
+        movement = GetComponent<movement>();
+        rocketAudio = GetComponent<AudioSource>();
     }
     void Update(){
         
     }
 
     void OnCollisionEnter(Collision other) {
+        if(crashedOrFinished) {return;}
+
         switch(other.gameObject.tag) {
             case "friend":
                 Debug.Log("Amigo");
                 break;
             case "Finish":
-                Debug.Log("Finished");
+                nextLevelSequence();
                 break;
             default:
-                reloadLevel();
+                crashSequence();
                 break;
         }
     }
+
+    void crashSequence() {
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(crashSound, 0.3f);
+        crashedOrFinished = true;
+        movement.enabled = false;
+        Invoke("reloadLevel", 2f);
+    }
+
+    void nextLevelSequence() {
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(winSound);
+        movement.enabled = false;
+        Invoke("loadNextLevel", 2f);
+    }
+
     void reloadLevel() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void loadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        int allScenesQuantity = SceneManager.sceneCountInBuildSettings;
+
+        if(nextSceneIndex < allScenesQuantity) {
+            SceneManager.LoadScene(nextSceneIndex);
+        } else {
+            SceneManager.LoadScene(0);
+        }
+        
     }
 }
