0250e06e3b5bf0b7c991b4ca264ea58254d827cc
Lukas Rodrigues <lukkasdev@gmail.com>
2023-12-22 07:14:22 -0300

    CollisionHandler Modification
    Primeira Modificação do dia 22/12/2023

--- RocketBoost/Assets/scripts/collisionHandler.cs
+++ RocketBoost/Assets/scripts/collisionHandler.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class collisionHandler : MonoBehaviour
-{
+public class collisionHandler : MonoBehaviour{
+
     void Start(){
-          
+
     }
     void Update(){
         
+    }
+
+    void OnCollisionEnter(Collision other) {
+        switch(other.gameObject.tag) {
+            case "friend":
+                Debug.Log("Amigo");
+                break;
+            case "Finish":
+                Debug.Log("Finished");
+                break;
+            default:
+                reloadLevel();
+                break;
+        }
+    }
+    void reloadLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
